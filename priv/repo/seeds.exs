c10b3eb9617ce3abc0aa3fccf9380c5064932661
Aliaksei Loi <aliaksei.loi@itechart-group.com>
2020-09-11 10:19:33 +0300

    add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,44 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Reddit.Repo.insert!(%Reddit.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Reddit.Repo
+alias Reddit.Auth.User
+alias Reddit.Articles.Post
+
+# Users
+
+{:ok, first_user} =
+  %User{
+    username: "sideswipeloi",
+    email: "sideswipe@gmail.com",
+    password_hash: Comeonin.Bcrypt.hashpwsalt("password")
+  }
+  |> Repo.insert()
+
+{:ok, second_user} =
+  %User{
+    username: "alex",
+    email: "alexloi@gmail.com",
+    password_hash: Comeonin.Bcrypt.hashpwsalt("password")
+  }
+  |> Repo.insert()
+
+# Posts
+
+%Post{
+  title: "Transformers",
+  description: "Cool robots you know",
+  user_id: first_user.id
+}
+|> Repo.insert()
+
+%Post{
+  title: "Inseption",
+  description: "I wanna sleep",
+  user_id: first_user.id
+}
+|> Repo.insert()
+
+%Post{
+  title: "Star Wars",
+  description: "I am your father",
+  user_id: second_user.id
+}
+|> Repo.insert()
