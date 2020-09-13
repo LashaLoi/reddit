dfde83fe4702f185ca7ca74e347f9b2c5f9d3d2d
Aliaksei Loi <aliaksei.loi@itechart-group.com>
2020-09-13 16:03:55 +0300

    add comments table

---
+++ lib/reddit/comments/comment.ex
@@ -0,0 +1,17 @@
+defmodule Reddit.Comments.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "comments" do
+    field :text, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:text])
+    |> validate_required([:text])
+  end
+end
